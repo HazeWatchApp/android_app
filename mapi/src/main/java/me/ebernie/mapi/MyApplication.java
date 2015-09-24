d31362e81c2c62fc0d9b41112e323ba830d2b4c2
Bernie Eng <ebernie@gmail.com>
2015-09-25 01:02:16 +0800

    Added crashlytics

--- mapi/src/main/java/me/ebernie/mapi/MyApplication.java
+++ mapi/src/main/java/me/ebernie/mapi/MyApplication.java
@@ -2,17 +2,29 @@ package me.ebernie.mapi;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
+
+import io.fabric.sdk.android.Fabric;
+import my.codeandroid.hazewatch.BuildConfig;
 import my.codeandroid.hazewatch.R;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 /**
- * Created by andhie on 9/19/15.
+ * Custom Application class
  */
 public class MyApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        // Set up Crashlytics, disabled for debug builds
+        Crashlytics cKit = new Crashlytics.Builder()
+                .core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+                .build();
+
+        // Initialize Fabric with the debug-disabled crashlytics.
+        Fabric.with(this, cKit);
 
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
                         .setDefaultFontPath(getString(R.string.font_regular))
