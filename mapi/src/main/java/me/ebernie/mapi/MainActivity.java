4b70ce7e0bdb2b096602a11597d2c561c813cd6c
Nazrul Kamaruddin <nazroll@gmail.com>
2015-10-05 01:39:12 +0800

    Based on @ndrlabs advice, I looked up for the comments that mentions about Crashlytics usage. Removed the init in MainActivity in favor of the init in MyApplication.

--- mapi/src/main/java/me/ebernie/mapi/MainActivity.java
+++ mapi/src/main/java/me/ebernie/mapi/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.actionbar_toolbar);
