f2ae67f9a758d57f7319933e640c587e5abc7820
Nazrul Kamaruddin <nazroll@gmail.com>
2015-10-05 01:22:55 +0800

    Fixed Crashlytics error when retrieving settings file.

--- mapi/src/main/java/me/ebernie/mapi/MainActivity.java
+++ mapi/src/main/java/me/ebernie/mapi/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 import me.ebernie.mapi.util.AnalyticsManager;
 import me.ebernie.mapi.util.LocationHelper;
 import my.codeandroid.hazewatch.R;
@@ -22,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.actionbar_toolbar);
